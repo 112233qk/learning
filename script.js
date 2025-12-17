9f1918b3e5ea08f4d74182e02f95098b19bf8b56
112233qk <kopetsatrio01@gmail.comgit config --global user.name 112233qkgit config --global user.email kopetsatrio01@gmail.com>
2025-12-17 20:32:50 +0700

    arithmetic operator
    adding arithmetic operator &
    making previous script into comment

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+/*
+//data type
+
+//string
 let userName = "Qk";
+
+//number
 let age = 76;
+
+//boolean
 let gender = true;
 
 console.log(typeof userName);
@@ -14,3 +22,43 @@ console.log(`and my gender is ${gender}`);
 document.getElementById("userName").textContent = `my name is ${userName}`;
 document.getElementById("age").textContent = `my age is ${age}`;
 document.getElementById("gender").textContent = `my age is ${gender}`;
+*/
+
+//arithmetic operator
+let students = 30;
+
+let addition = students + 2;
+let subtraction = students - 2;
+let multiplication = students * 2;
+let division = students / 2;
+let exponent = students ** 2;
+let modulus = students % 2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(exponent);
+console.log(modulus);
+
+//more simple way to use arimathic operator
+students += 2;
+students -= 2;
+students *= 2;
+students /= 2;
+students **= 2;
+students %= 2;
+
+//increment & decrement
+students++;
+students--;
+
+//operator precedence
+//starting from left to right
+//    1. parenthesis ()
+//    2. exponent
+//    3. multiplication & division & modulus
+//    4.addition $ subraction
+
+let precedence = 1 + 2 - 2 * (10 - 2);
+console.log(precedence);
